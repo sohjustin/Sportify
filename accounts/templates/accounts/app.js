cf42bdf8ca56334713dbd0e0015e69b8363b1218
sohjustin <sohjustin01@gmail.com>
2021-05-28 22:48:47 +0800

    add alert showing staff account details

---
+++ accounts/templates/accounts/app.js
@@ -0,0 +1,1 @@
+alert("Hi! Welcome to Sportify. When you sign up, you will have limited access as a customer. Here is my staff account so you can see more of this website features.  -sohjustin\r\n\r\n Staff Account: \r\n ID: Admin\r\n PW: sohjustin")
